f6ec3aa9525fdbea6224e31dc81715fec718b885
alekseyzvyagintsev <alex0236889@gmail.com>
2025-07-10 20:57:37 +0300

    Создан superuser и зарегистрированы модели в файле aadmin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+# admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    list_filter = ('name', 'id')
+    search_fields = ('name', 'description',)
+# admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+    list_filter = ('price', 'id')
+    search_fields = ('name', 'category',)
