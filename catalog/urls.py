ffc7b008b74ec502c142c8a6b657225c481f2bfc
alekseyzvyagintsev <alex0236889@gmail.com>
2025-08-11 17:23:21 +0300

    Выполнены все задания ДЗ

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,12 @@ from .views import (ProductCreateView,
                     ProductDetailView,
                     ProductUpdateView,
                     HomeListView,
-                    ContactView)
+                    ContactView,
+                    CategoryCreateView,
+                    CategoryUpdateView,
+                    CategoryDeleteView,
+                    CategoryDetailView,
+                    CategoryListView)
 
 app_name = CatalogConfig.name
 
@@ -14,8 +19,15 @@ urlpatterns = [
     path('contact/', ContactView.as_view(), name='contact'),
     path('', HomeListView.as_view(), name='home'),
     path('home/', HomeListView.as_view(), name='home'),
+
+    path('category/mew/', CategoryCreateView.as_view(), name='create_category'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
+    path('category/detail/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+
     path('product/new/', ProductCreateView.as_view(), name='create_product'),
-    path('product/list/', ProductListView.as_view(), name='product_list'),
+    path('products/', ProductListView.as_view(), name='products'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
